2642a298644277461dd94e235a0aeb0e9f1f423b
Pranav Sinha <pranavsinha922@gmail.com>
2023-08-27 23:49:48 +0530

    Create Fibonacci Memoization.js

---
+++ 1. Fibonacci Memoization/Fibonacci Memoization.js
@@ -0,0 +1,20 @@
+const fib = (n) => {
+    if (n <= 2)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+};  // too slow : takes O(2 ^ n) time and space
+
+// memoization (hashmap equivalent or any other fast access data-structure)
+// js obj, keys will be argument to the function and keys will be the return value
+
+const optimized_fib = (n, memo = {}) => {   // smooth as butter
+    if (n in memo) 
+        return memo[n];
+    if (n <= 2)
+        return 1;
+
+    memo[n] = optimized_fib(n - 1, memo) + optimized_fib(n - 2, memo);
+    return memo[n];
+};
+
+console.log(optimized_fib(1476)); // 1476 is the limit for my pc. yours may vary
